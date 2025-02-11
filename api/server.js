1beadc08bc5c2e9973ad533291ea45a50e922205
Davi Santos <davi15santos08@gmail.com>
2025-02-10 21:33:06 -0300

    feat: rota da temperatura

--- api/server.js
+++ api/server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { checkWeekday } = require("../middlewares/checkWeekday");
-const authenticateToken = require("../middlewares/authenticateToken");
+const http = require("http");
+const { Server } = require("socket.io");
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
 app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -17,7 +25,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log("Conectado ao MongoDB"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("Erro ao conectar:", err));
+
+const { checkWeekday } = require("../middlewares/checkWeekday");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 app.use("/logar", require("../routes/auth"));
 app.use("/create", require("../routes/user"));
@@ -28,9 +39,23 @@ app.use(
   require("../routes/laboratorio")
 );
 
+io.on("connection", (socket) => {
+  console.log("Usuário conectado");
+
+  socket.on("novaTemperatura", (temp) => {
+    console.log(`Nova temperatura recebida: ${temp}`);
+    temperaturaAtual = temp; 
+    io.emit("atualizarTemperatura", temp); 
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuário desconectado");
+  });
+});
+
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
 }
